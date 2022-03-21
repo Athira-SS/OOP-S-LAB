8da830a7b3c13b8962252cc41ffb69eda910884f
Athira-SS <97280088+Athira-SS@users.noreply.github.com>
2022-03-21 15:25:05 +0530

    Add files via upload

---
+++ details.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class details
+{
+	public static void main(String[] args)
+	{
+		String name;
+		int age;
+		float salary;
+		Scanner s1=new Scanner(System.in);
+		System.out.println("enter the name");
+		name=s1.nextLine();
+		System.out.println("enter the age");
+		age=s1.nextInt();
+		System.out.println("enter the salary");
+		salary=s1.nextFloat();
+		System.out.println("____________________");
+		System.out.println("\t\t\nName:"+name+"\t\t\n Age:"+age+"\t\t\nSalary"+salary"\n\n");
+		System.out.println("____________________");
+	}
+}
+		
