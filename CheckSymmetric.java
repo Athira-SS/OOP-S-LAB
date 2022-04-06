d1a54a4ade0a3a01423e8ec29a57f25961d23e38
Athira-SS <97280088+Athira-SS@users.noreply.github.com>
2022-04-06 11:33:32 +0530

    Update CheckSymmetric.java

--- CheckSymmetric.java
+++ CheckSymmetric.java
@@ -1,37 +1,42 @@
 import java.util.*;
-class CheckSymmetric
+public class CheckSymmetric {
+public static void main(String[] args) 
 {
-	public static void main(String[] args)
-	{
-		int a[][]=new int[10][10];
-		int b[][]=new int[10][10],i,j,n,m;
-		int flag=0;
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter the size of matrix");
-		n=s.nextInt();
-		m=s.nextInt();
-		System.out.println("Enter the elements ");
-		for(i=0;i<n;i++)
-		    for(j=0;j<m;j++)
-			a[i][j]=s.nextInt();
-		for(i=0;i<m;i++)
-		    for(j=0;j<n;j++)
-			b[i][j]=a[j][i];
-		for(i=0;i<n;i++)
-		    for(j=0;j<m;j++)
-		    {
-			if(a[i][j]==b[i][j])
-			   flag=1;
-			else
-			   flag=0;
-
-		    }
-		if(flag==0)
-			 System.out.println("matrix is nonsymmetric");
-		else
-			 System.out.println("matrix is symmetric");
-	}
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the Number of rows of the Matrix");
+    int row = sc.nextInt();
+    System.out.println("Enter the Number of Columns of the Matrix");
+    int col = sc.nextInt();
+    int matrix[][] = new int[row][col];
+    int i,j;
+    boolean state=true;
+    for(i=0;i<row;i++){
+            for(j=0;j<col;j++){
+                   System.out.println("Enter the Element at M("+i+","+j+")");
+                   matrix[i][j] = sc.nextInt();
+    }
 }
+for(i=0;i<row;i++)
+{
+    for(j=0;j<col;j++)
+    {
+       if(matrix[i][j]!=matrix[j][i]){
+           state=false;
+           break;
+       }
+    }
+}
+if(state)
+{
+    System.out.println("Matrix is Symmetric");
+}
+else
+{
+   System.out.println("Matrix is Antisymmetric");
+}
+}
+}
+
 			   
 		
 		
